d76ec34867cf12dc9c697fe5e53d7a794b47293c
Marcin <55813873+percit@users.noreply.github.com>
2021-02-14 22:29:27 +0100

    Create romanToInt.cpp

---
+++ Numbers/romanToInt.cpp
@@ -0,0 +1,59 @@
+int romanToInt(const std::string& data) {
+    int value = 0;
+    int i = 0;
+    for (i; i < data.size(); ++i) {
+        switch (data.at(i)) {
+            case 'M':
+                value += 1000;
+                break;
+            case 'D':
+                value += 500;
+                break;
+            case 'C':
+                if((i+1) < data.size()) {
+                    if ((data.at(i + 1) == 'D' || data.at(i + 1) == 'M')) {
+                        value -= 100;
+                    }
+                    else {
+                        value += 1;
+                    }
+                }else {
+                    value += 100;
+                }
+                break;
+            case 'L':
+                value += 50;
+                break;
+            case 'X':
+                if((i+1) < data.size()) {
+                    if (data.at(i + 1) == 'L' || data.at(i + 1) == 'C') {
+                        value -= 10;
+                    }
+                    else {
+                        value += 1;
+                    }
+                }else {
+                    value += 10;
+                }
+                break;
+            case 'V':
+                value += 5;
+                break;
+            case 'I':
+                if((i+1) < data.size()) {
+                    if (data.at(i + 1) == 'V' || data.at(i + 1) == 'X') {
+                        value -= 1;
+                    } else {
+                        value += 1;
+                    }
+                }else{
+                    value +=1;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    return value;
+}
