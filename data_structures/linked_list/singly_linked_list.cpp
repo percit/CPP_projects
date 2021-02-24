755873f343c20909030636d5f0745d44818ef869
Marcin <55813873+percit@users.noreply.github.com>
2021-02-24 23:59:42 +0100

    Update singly_linked_list.cpp

--- data_structures/linked_list/singly_linked_list.cpp
+++ data_structures/linked_list/singly_linked_list.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 struct listElement{
     listElement *next;
     char data;
@@ -34,14 +33,100 @@ void l_pop_front ( listElement *&head){
         delete p;
     }
 }
+void l_push_back(listElement *&head, char v){
+    listElement *p, *e;
+    e = new listElement;//making new element
+    e->next = NULL; //initialisation
+    e->data = v;
+    p = head;
+    if(p){
+        while(p->next){
+            p = p->next;
+        }
+        p->next = e;
+    }
+    else{
+        head = e;
+    }
+}
+void l_pop_back(listElement *& head){
+    listElement *p = head;
+    if(p){
+        if(p->next){
+            while(p->next->next) p = p->next;
+            delete p->next;
+            p->next - NULL;
+        }
+        else{
+            delete p; //if one element in list
+            head = NULL;
+        }
+    }
+}
+void l_insert_before(listElement *&head, listElement *e, char v){
+    listElement *p = head;
+    if(p == e){
+        l_push_front(head, v);
+    }
+    else{
+        while(p->next != e){
+            p = p->next;
+            p->next = new listElement;
+            p->next->next = e;
+            p->next->data = v;
+        }
+    }
+}
+void l_insert_after(listElement *e, char v){
+    listElement *p = new listElement;
+    p->next = e->next;//p bedzie nastepne po e, a potem juz izi
+    p->data = v;
+    e->next = p;
+}
+void l_remove(listElement *&head, listElement *e){
+    listElement *p;
+    if(head == e){
+        l_pop_front(head);
+    }
+    else{
+        p = head;
+        while(p->next != e){
+            p = p->next;
+        }
+        p->next = e->next;
+        delete e;
+    }
+}
 int main(){
 
-    listElement *L = NULL;
-    l_push_front(L, 'a');
-    l_push_front(L, 'b');
-    l_push_front(L, 'c');
-    l_print(L);
-    l_pop_front(L);
-    l_print(L);
+  listElement * L = NULL; // zawiera adres początku listy
+  listElement * e;        // do wskazywania elementów listy
+  char i;
+
+  for( i = 'A'; i <= 'G'; i++ ) l_push_back ( L, i );
+  l_print ( L );
+
+  // Przechodzimy do elementu o wartości D
+
+  e = L;
+
+  for( i = 'A'; i <= 'C'; i++ ) e = e->next;
+
+  // Przed elementem D umieszczamy +
+
+  l_insert_before ( L, e, '+' );
+
+  // Za elementem D umieszczamy *
+
+  l_insert_after ( e, '*' );
+  l_print ( L );
+
+  // Usuwamy element D oraz element pierwszy i ostatni
+
+  l_remove ( L, e );
+  l_pop_front ( L );
+  l_pop_back ( L );
+  l_print ( L );
+
     return 0;
 }
