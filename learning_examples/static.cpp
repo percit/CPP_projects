2f93c515b7342e1ec4adaf7abdf9459d85e0f4fd
Marcin <55813873+percit@users.noreply.github.com>
2021-03-17 10:27:30 +0100

    Update static.cpp

--- learning_examples/static.cpp
+++ learning_examples/static.cpp
@@ -2,16 +2,16 @@
  
 void incrementAndPrint()
 {
-    int value{ 1 }; // automatic duration by default
-    ++value;
-    std::cout << value << '\n';
-} // value is destroyed here
+    static int s_value{ 1 }; // static duration via static keyword.  This initializer is only executed once.
+    ++s_value;
+    std::cout << s_value << '\n';
+} // s_value is not destroyed here, but becomes inaccessible because it goes out of scope
  
 int main()
 {
-    incrementAndPrint();
-    incrementAndPrint();
-    incrementAndPrint();
+    incrementAndPrint(); //2
+    incrementAndPrint();//3
+    incrementAndPrint();//4
  
     return 0;
 }
