6cdf716219c1d65e7cc15db3653db19e1b150537
Marcin <55813873+percit@users.noreply.github.com>
2021-03-17 18:36:04 +0100

    Create enum.cpp

---
+++ learning_examples/enum.cpp
@@ -0,0 +1,14 @@
+enum Color
+{
+    color_black, // assigned 0
+    color_red, // assigned 1
+    color_blue, // assigned 2
+    color_green, // assigned 3
+    color_white, // assigned 4
+    color_cyan, // assigned 5
+    color_yellow, // assigned 6
+    color_magenta // assigned 7
+};
+ 
+Color paint{ color_white };
+std::cout << paint;
