c0b388fdfbceb88e8dd6136ba4f41245cd4bc431
Marcin Zdunek <55813873+percit@users.noreply.github.com>
2021-04-02 22:41:45 +0200

    Create virtual.cpp

---
+++ learning_examples/classes/virtual.cpp
@@ -0,0 +1,25 @@
+class A
+{
+public:
+	virtual const char* getName1(int x) { return "A"; }
+	virtual const char* getName2(int x) { return "A"; }
+	virtual const char* getName3(int x) { return "A"; }
+};
+ 
+class B : public A
+{
+public:
+	virtual const char* getName1(short int x) override { return "B"; } // compile error, function is not an override
+	virtual const char* getName2(int x) const override { return "B"; } // compile error, function is not an override
+	virtual const char* getName3(int x) override { return "B"; } // okay, function is an override of A::getName3(int)
+ 
+};
+ 
+int main()
+{
+	return 0;
+}
+
+//how to check that we override the function, if both functions aren’t the same, then we have an error, use it frequently with virtual funcitions
+
+//As with normal virtual member functions, if a base class function is virtual, all derived overrides will be considered virtual regardless of whether they are specified as such. It is not necessary to create an empty derived class destructor just to mark it as virtual.
