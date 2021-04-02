30918e382ded24b5013cfa4b2d029a25dbbe27d0
Marcin Zdunek <55813873+percit@users.noreply.github.com>
2021-04-02 22:50:11 +0200

    Update output.cpp

--- learning_examples/text-input-output/output.cpp
+++ learning_examples/text-input-output/output.cpp
@@ -21,3 +21,28 @@ std::cout << std::dec << 29 << '\n'; // back to decimal
 
 to sa manipulatory, sa madrzejsze od flag
 
+
+
+1
+2
+3
+4
+5
+6
+7
+std::string sSource{ "sphinx of black quartz, judge my vow" };
+ 
+char szBuf[20];
+int nLength{ static_cast<int>(sSource.copy(szBuf, 5, 10)) };
+szBuf[nLength] = '\0';  // Make sure we terminate the string in the buffer
+ 
+std::cout << szBuf << '\n';
+
+Output:
+black
+kopiowanie tekstu, sprawdzamy jego dlugosc i jednoczesnie kopiujemy w jednej linii, zeby dodac na koniec \0 tak jak pan bog nakazal
+
+
+
+
+
